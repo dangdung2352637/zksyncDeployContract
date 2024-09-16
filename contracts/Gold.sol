69c550f0501b13ab38dfafc106e208d6711e2c75
dangdung2352637 <dangdungone.gmail.com>
2024-09-16 20:54:38 +0700

    sencond commit

--- contracts/Gold.sol
+++ contracts/Gold.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GoldToken is ERC20 {
-    constructor(uint256 totalSupply) ERC20("GOLD", "GOLD") {
+contract Leopad is ERC20 {
+    constructor(uint256 totalSupply) ERC20("NEP", "NEP") {
         _mint(msg.sender, totalSupply);
     }
 }
--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,7 +5,7 @@ import { deployContract } from "./utils";
 // It will deploy a Greeter contract to selected network
 // as well as verify it on Block Explorer if possible for the network
 export default async function () {
-  const contractArtifactName = "GoldToken";
+  const contractArtifactName = "Leopad";
   const constructorArguments = [ethers.parseEther('10000').toString()];
   await deployContract(contractArtifactName, constructorArguments);
 }
